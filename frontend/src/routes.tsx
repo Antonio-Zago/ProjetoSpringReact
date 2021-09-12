05c012e93cae606ba93e35ff65d3b04755fbecc4
Antonio-Zago <antoniozagodev@gmail.com>
2021-09-12 18:52:41 -0300

    Routes

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,21 @@
+import Dashboard from 'pages/dashboards';
+import Home from 'pages/home';
+import  {BrowserRouter, Switch, Route} from 'react-router-dom';
+
+function Routes(){
+    return (
+        <BrowserRouter>
+
+            <Switch>
+                <Route path="/" exact>
+                    <Home/>
+                </Route>
+                <Route path="/dashboard">
+                    <Dashboard/>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
