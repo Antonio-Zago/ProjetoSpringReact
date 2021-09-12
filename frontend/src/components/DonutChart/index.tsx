caee78b83e61211bcc002aaba0b20fa61d41b23b
Antonio-Zago <antoniozagodev@gmail.com>
2021-09-12 19:34:26 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/requests';
@@ -6,35 +7,38 @@ import { BASE_URL } from 'utils/requests';
 
 type ChartData = {
     labels: string[];
-    series:number[];    
+    series: number[];
 }
 
 function DonutChart() {
 
-    let chartData: ChartData = {
-        labels : [],
-        series : []
-    }
+    const [chartData, setChartData] = useState<ChartData>({
+        labels: [],
+        series: []
+    });
 
-    axios.get(BASE_URL + '/sales/sum-by-seller')
-        .then((response) => {
+    useEffect(() => {
+        axios.get(BASE_URL + '/sales/sum-by-seller')
+            .then((response) => {
 
-            const data = response.data as  SaleSum[];
-            const myLabels = data.map(x =>x.sellerName);
-            const mySeries = data.map(x => x.sum)
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum)
 
-            chartData = {
-                labels : myLabels,
-                series : mySeries
-            }
-            console.log(chartData)
-        })
+                setChartData({
+                    labels: myLabels,
+                    series: mySeries
+                });
+            })
+    }, [])
+
+
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     //}
-    
+
     const options = {
         legend: {
             show: true
@@ -42,14 +46,14 @@ function DonutChart() {
     }
 
     return (
-        <Chart 
-            options={{...options, labels:chartData.labels}} //... significa que posso inserir mais objetos 
+        <Chart
+            options={{ ...options, labels: chartData.labels }} //... significa que posso inserir mais objetos 
             series={chartData.series}
             type="donut"
             height="240"
         /> //Componente do apex charts
     )
-        
+
 }
 
 export default DonutChart;
